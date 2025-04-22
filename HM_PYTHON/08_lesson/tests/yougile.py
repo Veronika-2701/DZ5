2ddff1c23e43859945c1030db36d88624a768154
Veronika-2701 <niko160727@gmail.com>
2025-04-22 20:18:10 +0300

    upd

--- HM_PYTHON/08_lesson/tests/yougile.py
+++ HM_PYTHON/08_lesson/tests/yougile.py
@@ -3,12 +3,18 @@ import requests
 class YouGile:
     def __init__(self, base_url) -> None:
         self.base_url = base_url
-        self.token = 'токен'
+        self.token = 'токен '
 
     def req_get(self, req_type="GET", url="", data={}):
         headers = {'Authorization': 'Bearer '+ self.token, 'Accept': 'application/json'}
     #    return requests.get(self.base_url + 'projects/'+id, headers=headers)
         return requests.request(req_type, self.base_url + url, headers=headers, json=data)
     
+    def req_post(self, req_type="POST", url="", data={}):
+        headers = {'Authorization': 'Bearer '+ self.token, 'Accept': 'application/json'}
+        return requests.request(req_type, self.base_url + url, headers=headers, json=data)
     
+    def req_put(self, req_type="PUT", url="", data={}):
+        headers = {'Authorization': 'Bearer '+ self.token, 'Accept': 'application/json'}
+        return requests.request(req_type, self.base_url + url, headers=headers, json=data)
     
